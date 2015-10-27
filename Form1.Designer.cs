4729d3978d332ecb62fce48e7b4c30f07dbb6a75
exelix11 <ex.elix@libero.it>
2015-10-27 20:14:05 +0100

    First commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace _3DlandMSBTeditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.version01ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(445, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.exitToolStripMenuItem,
+            this.toolStripSeparator2,
+            this.version01ToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // loadToolStripMenuItem
+            // 
+            this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            this.loadToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.loadToolStripMenuItem.Text = "Load";
+            this.loadToolStripMenuItem.Click += new System.EventHandler(this.loadToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(97, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 40);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(88, 316);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(240, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Strings in the list:                                   Edit string:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(106, 40);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(327, 315);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(149, 6);
+            // 
+            // version01ToolStripMenuItem
+            // 
+            this.version01ToolStripMenuItem.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.version01ToolStripMenuItem.Name = "version01ToolStripMenuItem";
+            this.version01ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.version01ToolStripMenuItem.Text = "Version 0.1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(445, 367);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "3D Land text editor";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem version01ToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace _3DlandMSBTeditor
+{
+    public partial class Form1 : Form
+    {
+        List<List<byte>> Strings = new List<List<byte>>();
+        string LoadedFile = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void loadFile(string file)
+        {
+            Strings.Clear();
+            listBox1.Items.Clear();
+            byte[] byteBuffer = File.ReadAllBytes(file);
+            string byteBufferAsString = System.Text.Encoding.UTF8.GetString(byteBuffer);
+            int base_offset = byteBufferAsString.IndexOf("TXT2");
+            base_offset += 4;
+            BinaryReader bin = new BinaryReader(File.Open(file, FileMode.Open));
+            bin.BaseStream.Position = base_offset;
+            int SectionSize = bin.ReadInt32(); //0x4
+            bin.BaseStream.Position += 8;
+            long countpos = bin.BaseStream.Position;
+            int count = bin.ReadInt32();
+            List<int> addressList = new List<int>();
+            for (int i = 0; i < count; i++)
+            {
+                addressList.Add(bin.ReadInt32());
+            }
+            for (int i = 0; i < count; i++)
+            {
+                bin.BaseStream.Position = countpos  + addressList[i] ;
+                Debug.Print("POS: " + (countpos + addressList[i] ).ToString());
+                List<byte> String = new List<byte>();
+                bool FindNULL = false;
+                while (!FindNULL)
+                {
+                    try {
+                        byte[] temp = bin.ReadBytes(2);
+                        Debug.Print(temp[0].ToString() + " " + temp[1].ToString());
+                        if (IsNull(temp)) FindNULL = true; else String.AddRange(temp);
+                    }
+                    catch 
+                    {
+                        FindNULL = true;
+                    }
+                }
+                Strings.Add(String);
+            }
+            for (int i = 0; i < Strings.Count; i++) listBox1.Items.Add(i);
+            LoadedFile = file;
+            bin.Close();
+        }
+
+        void WriteFile(string file)
+        {
+            if (file != LoadedFile) File.Copy(LoadedFile, file);
+            byte[] byteBuffer = File.ReadAllBytes(file);
+            string byteBufferAsString = System.Text.Encoding.UTF8.GetString(byteBuffer);
+            int base_offset = byteBufferAsString.IndexOf("TXT2");
+            base_offset += 4;
+            BinaryWriter bin = new BinaryWriter(File.Open(file, FileMode.Open));
+            bin.BaseStream.Position = base_offset;
+            long SectionSizePos = bin.BaseStream.Position;
+            bin.Write(new byte[4]); //0x4
+            bin.Write(new byte[8]);
+            long CountPos = bin.BaseStream.Position;
+            bin.Write(Strings.Count); //COUNT
+            long AddressPos = bin.BaseStream.Position;
+            for (int i = 0; i < Strings.Count;i ++) bin.Write(new byte[4]);
+            List<int> Pos = new List<int>();
+            for (int i = 0; i < Strings.Count; i++)
+            {
+                Pos.Add((Int32)(bin.BaseStream.Position - CountPos ));
+                bin.Write(Strings[i].ToArray());
+                bin.Write(new byte[2]);
+            }
+            bin.BaseStream.Position = AddressPos;
+            for (int i = 0; i < Strings.Count; i++)
+            {
+                bin.Write(Pos[i]);
+            }
+            bin.BaseStream.Position = SectionSizePos;
+            bin.Write(bin.BaseStream.Length - CountPos);
+            bin.Flush();
+            bin.Close();
+        }
+
+        private bool IsNull(byte[] input)
+        {
+            if (input.Length > 1)
+            {
+                int ZeroCount = 0;
+                for (int i = 0; i < input.Length; i++)
+                {
+                    if (input[i] == 0x00) ZeroCount++;
+                }
+                if (ZeroCount > 1) return true; else return false;
+            }
+            else return true;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void loadToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog load = new OpenFileDialog();
+            if (load.ShowDialog() != DialogResult.OK) return;
+            loadFile(load.FileName);
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = System.Text.Encoding.Unicode.GetString(Strings[listBox1.SelectedIndex].ToArray());
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog Save = new SaveFileDialog();
+            if (Save.ShowDialog() != DialogResult.OK) return;
+            WriteFile(Save.FileName);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            Strings[listBox1.SelectedIndex].Clear();
+            Strings[listBox1.SelectedIndex].AddRange(System.Text.Encoding.Unicode.GetBytes(textBox1.Text));
+        }
+    }
+}
